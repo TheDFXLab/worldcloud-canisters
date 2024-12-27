9bd9970eb20039b02523f384111005aaf560515b
georgiod9 <georgiod9@gmail.com>
2024-12-27 19:02:09 +0400

    Refactor frontend structure and update dependencies
    - Replaced main.js with main.jsx to support JSX syntax.
    - Updated index.html to streamline structure and ensure proper rendering.
    - Added Vite plugin for React to enhance development experience.
    - Updated package.json to include React and ReactDOM dependencies.
    - Introduced assetstorage.wasm and assetstorage.wasm.gz for asset management.
    - Enhanced development scripts and added type definitions for better type safety.

--- src/migrator-management-canister-frontend/vite.config.js
+++ src/migrator-management-canister-frontend/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import { fileURLToPath, URL } from 'url';
 import environment from 'vite-plugin-environment';
+import react from '@vitejs/plugin-react';
 import dotenv from 'dotenv';
 
 dotenv.config({ path: '../../.env' });
@@ -11,6 +12,9 @@ export default defineConfig({
   },
   optimizeDeps: {
     esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
       define: {
         global: "globalThis",
       },
@@ -26,6 +30,7 @@ export default defineConfig({
   },
   publicDir: "assets",
   plugins: [
+    react(),
     environment("all", { prefix: "CANISTER_" }),
     environment("all", { prefix: "DFX_" }),
   ],
