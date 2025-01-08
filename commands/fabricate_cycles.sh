bb628d41460ff9a917351bf78c0c8f13f7afa95a
georgiod9 <georgiod9@gmail.com>
2025-01-08 19:18:44 +0400

    helper scripts for common dev dfx commands

---
+++ commands/fabricate_cycles.sh
@@ -0,0 +1,10 @@
+
+echo "===========FABRICATE CYCLES==========="
+
+# AMOUNT
+echo ""
+echo "===========AMOUNT IN ICP==========="
+
+CANISTER_ID="be2us-64aaa-aaaaa-qaabq-cai"
+AMOUNT_IN_ICP_TOKEN="10"
+dfx ledger fabricate-cycles --canister $CANISTER_ID --amount $AMOUNT_IN_ICP_TOKEN
