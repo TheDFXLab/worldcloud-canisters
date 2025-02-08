f31f457b8a597fc4fc2f3407431e35a670626b3a
georgiod9 <georgiod9@gmail.com>
2025-02-08 10:43:01 +0400

    implement cycles provider for managing and monitoring canister cycles

---
+++ src/migrator-management-canister-frontend/src/context/CyclesContext/CyclesContext.tsx
@@ -0,0 +1,130 @@
+import {
+  createContext,
+  useContext,
+  useState,
+  ReactNode,
+  useEffect,
+} from "react";
+import { useIdentity } from "../IdentityContext/IdentityContext";
+import CyclesApi from "../../api/cycles";
+import { Principal } from "@dfinity/principal";
+import { CanisterStatusResponse } from "../../../../declarations/migrator-management-canister-backend/migrator-management-canister-backend.did";
+import MainApi from "../../api/main";
+
+interface CyclesContextType {
+  isLoadingCycles: boolean;
+  cyclesAvailable: number;
+  currentCanisterId: Principal | null;
+  setCurrentCanisterId: (canisterId: Principal) => void;
+  canisterStatus: CanisterStatusResponse | null;
+  estimateCycles: (amountInIcp: number) => Promise<number>;
+  getStatus: (canisterId: string) => Promise<void>;
+  cyclesStatus: CanisterStatusResponse | null;
+  cyclesRate: number;
+}
+
+const CyclesContext = createContext<CyclesContextType | undefined>(undefined);
+
+export function CyclesProvider({ children }: { children: ReactNode }) {
+  /*Hooks*/
+  const { identity } = useIdentity();
+
+  /** States */
+  const [isLoadingCycles, setIsLoadingCycles] = useState<boolean>(false);
+  const [cyclesAvailable, setCyclesAvailable] = useState<number>(0);
+  const [currentCanisterId, setCurrentCanisterId] = useState<Principal | null>(
+    null
+  );
+  const [canisterStatus, setCanisterStatus] =
+    useState<CanisterStatusResponse | null>(null);
+  const [isLoadingStatus, setIsLoadingStatus] = useState(false);
+  const [cyclesStatus, setCyclesStatus] =
+    useState<CanisterStatusResponse | null>(null);
+  const [cyclesRate, setCyclesRate] = useState<number>(0);
+
+  // Get maximum amount of cycles purchasable by user
+  useEffect(() => {
+    getCyclesToAdd();
+  }, []);
+
+  const getCyclesToAdd = async (amountInIcp?: number) => {
+    try {
+      setIsLoadingCycles(true);
+      const cyclesApi = new CyclesApi(identity);
+      const cycles = await cyclesApi.getCyclesToAdd(amountInIcp);
+      setCyclesAvailable(cycles);
+      return cycles;
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoadingCycles(false);
+    }
+  };
+
+  const estimateCycles = async (amountInIcp: number) => {
+    try {
+      if (cyclesRate) {
+        return cyclesRate * amountInIcp;
+      }
+
+      const cyclesApi = new CyclesApi(identity);
+      const cycles = await cyclesApi.estimateCyclesToAdd(amountInIcp);
+      setCyclesRate(cycles / amountInIcp);
+      return cycles;
+    } catch (error) {
+      console.log(error);
+      return 0;
+    }
+  };
+
+  const getStatus = async (canisterId: string) => {
+    try {
+      if (!canisterId) {
+        console.log(`Canister ID is not set`);
+        throw new Error("Canister ID is not set");
+      }
+
+      // const authApi = new AuthorityApi(Principal.fromText(canisterId));
+      const mainApi = await MainApi.create(identity);
+      if (!mainApi) {
+        throw new Error("Main API not created");
+      }
+
+      setIsLoadingStatus(true);
+      const result = await mainApi.getCanisterStatus(
+        Principal.fromText(canisterId)
+      );
+      setCyclesStatus(result);
+    } catch (error) {
+      console.log(`Error:`, error);
+      setIsLoadingStatus(false);
+    } finally {
+      setIsLoadingStatus(false);
+    }
+  };
+
+  return (
+    <CyclesContext.Provider
+      value={{
+        isLoadingCycles,
+        cyclesAvailable,
+        currentCanisterId,
+        setCurrentCanisterId,
+        canisterStatus,
+        estimateCycles,
+        getStatus,
+        cyclesStatus,
+        cyclesRate,
+      }}
+    >
+      {children}
+    </CyclesContext.Provider>
+  );
+}
+export function useCycles() {
+  const context = useContext(CyclesContext);
+  if (context === undefined) {
+    throw new Error("useCycles must be used within a CyclesProvider");
+  }
+  return context;
+}
