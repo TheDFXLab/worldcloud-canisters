74979d78e4dd228c33800efbb508836eec888a2f
georgiod9 <georgiod9@gmail.com>
2025-02-08 10:42:12 +0400

    implement and use get user ICP balance function

--- src/migrator-management-canister-frontend/src/context/LedgerContext/LedgerContext.tsx
+++ src/migrator-management-canister-frontend/src/context/LedgerContext/LedgerContext.tsx
@@ -5,6 +5,7 @@ import MainApi from "../../api/main";
 import { e8sToIcp } from "../../utility/e8s";
 
 interface LedgerContextType {
+  balance: bigint;
   transfer: (amount: number, to: string) => Promise<boolean>;
   isTransferring: boolean;
   pendingDeposits: number;
@@ -22,6 +23,7 @@ export const LedgerProvider: React.FC<{ children: React.ReactNode }> = ({
   const [isTransferring, setIsTransferring] = useState(false);
   const [error, setError] = useState<string | null>(null);
   const [pendingDeposits, setPendingDeposits] = useState<number>(0);
+  const [balance, setBalance] = useState<bigint>(BigInt(0));
 
   const getPendingDeposits = async () => {
     const mainApi = await MainApi.create(identity);
@@ -29,6 +31,15 @@ export const LedgerProvider: React.FC<{ children: React.ReactNode }> = ({
     if (pendingDeposits && pendingDeposits.e8s > 0) {
       setPendingDeposits(Number(pendingDeposits.e8s));
     }
+  };
+
+  const getBalance = async () => {
+    const ledgerApi = await LedgerApi.create(identity);
+    if (!ledgerApi) {
+      throw new Error("Ledger API not initialized");
+    }
+    const balance = await ledgerApi.getBalance();
+    setBalance(balance);
   };
 
   const transfer = async (amountInIcp: number, to: string) => {
@@ -52,11 +63,12 @@ export const LedgerProvider: React.FC<{ children: React.ReactNode }> = ({
 
   useEffect(() => {
     getPendingDeposits();
+    getBalance();
   }, []);
 
   return (
     <LedgerContext.Provider
-      value={{ transfer, isTransferring, error, pendingDeposits }}
+      value={{ transfer, isTransferring, error, pendingDeposits, balance }}
     >
       {children}
     </LedgerContext.Provider>
