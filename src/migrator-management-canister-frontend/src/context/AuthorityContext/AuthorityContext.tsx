f5728563d5c7f988ecbf8b6605963f41f0079bf2
georgiod9 <georgiod9@gmail.com>
2025-02-08 13:33:29 +0400

    debug refresh identity bug

--- src/migrator-management-canister-frontend/src/context/AuthorityContext/AuthorityContext.tsx
+++ src/migrator-management-canister-frontend/src/context/AuthorityContext/AuthorityContext.tsx
@@ -10,6 +10,7 @@ import AuthorityApi, { CanisterStatus } from "../../api/authority";
 import { decodeError } from "../../utility/errors";
 import { State } from "../../App";
 import { useIdentity } from "../IdentityContext/IdentityContext";
+import { useParams } from "react-router-dom";
 
 interface AuthorityContextType {
   status: CanisterStatus | null;
@@ -33,12 +34,16 @@ export function AuthorityProvider({
   const [status, setStatus] = useState<CanisterStatus | null>(null);
   const [isLoadingStatus, setIsLoadingStatus] = useState(true);
   const { identity } = useIdentity();
-
-  if (!state.canister_id) return;
-  const authApi = new AuthorityApi(Principal.fromText(state.canister_id));
+  const { canisterId } = useParams();
 
   const refreshStatus = async () => {
     try {
+      if (!canisterId) {
+        console.log(`Canister ID is not set`);
+        throw new Error("Canister ID is not set");
+      }
+      const authApi = new AuthorityApi(Principal.fromText(canisterId));
+
       setIsLoadingStatus(true);
       const result = await authApi.getCanisterStatus(
         authApi.canisterId,
@@ -46,6 +51,7 @@ export function AuthorityProvider({
       );
       setStatus(result);
     } catch (error) {
+      console.log(`Error:`, error);
       setIsLoadingStatus(false);
     } finally {
       setIsLoadingStatus(false);
@@ -54,6 +60,11 @@ export function AuthorityProvider({
 
   const handleAddController = async (newController: string) => {
     try {
+      if (!canisterId) {
+        throw new Error("Canister ID is not set");
+      }
+      const authApi = new AuthorityApi(Principal.fromText(canisterId));
+
       setIsLoadingStatus(true);
       // Implement add controller logic
       const result = await authApi.addController(
@@ -76,6 +87,11 @@ export function AuthorityProvider({
 
   const handleRemoveController = async (controller: string) => {
     try {
+      if (!canisterId) {
+        throw new Error("Canister ID is not set");
+      }
+      const authApi = new AuthorityApi(Principal.fromText(canisterId));
+
       setIsLoadingStatus(true);
 
       const result = await authApi.removeController(
@@ -96,6 +112,7 @@ export function AuthorityProvider({
     }
   };
 
+  // TODO: remove this and propagate changes in project
   useEffect(() => {
     refreshStatus();
   }, []);
--- src/migrator-management-canister-frontend/src/context/DeploymentContext/DeploymentContext.tsx
+++ src/migrator-management-canister-frontend/src/context/DeploymentContext/DeploymentContext.tsx
@@ -11,6 +11,7 @@ import { useIdentity } from "../IdentityContext/IdentityContext";
 import { Principal } from "@dfinity/principal";
 import { WorkflowRunDetails } from "../../../../declarations/migrator-management-canister-backend/migrator-management-canister-backend.did";
 import { useProgress } from "../ProgressBarContext/ProgressBarContext";
+import { internetIdentityConfig } from "../../config/config";
 
 export interface DeploymentDetails {
   workflow_run_id: number;
@@ -68,6 +69,13 @@ export function DeploymentsProvider({ children }: { children: ReactNode }) {
     try {
       if (!identity) {
         throw new Error("Identity not found");
+      }
+      if (
+        identity.getPrincipal().toText() ===
+        internetIdentityConfig.loggedOutPrincipal
+      ) {
+        console.log("Logged out, skipping deployment refresh");
+        return;
       }
 
       setIsLoadingProgress(true);
