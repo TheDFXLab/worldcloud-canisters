8e09de9bc5b7a63242a7ad991cac14e45ad7026c
georgiod9 <georgiod9@gmail.com>
2025-02-19 16:57:49 +0400

    replace "production" with "ic"

--- src/migrator-management-canister-frontend/src/config/config.ts
+++ src/migrator-management-canister-frontend/src/config/config.ts
@@ -1,13 +1,24 @@
 export const environment = process.env.DFX_NETWORK || "local";
-export const getCanisterUrl = (canisterId: string) => {
-    return environment === "production"
+export const getCanisterUrl = (canisterId: string, envOverride?: string) => {
+    let res = "";
+    if (envOverride) {
+        res = envOverride == "ic"
+            ? `https://${canisterId}.icp0.io`
+            : `http://${canisterId}.localhost:8000`;
+
+        console.log(`calling...`, res);
+    }
+
+    res = environment === "ic"
         ? `https://${canisterId}.icp0.io`
         : `http://${canisterId}.localhost:8000`;
+    console.log(`calling...`, res);
+    return res;
 };
 
-const replica_port = environment === "production" ? 4943 : 8000;
-const replica_host = environment === "production" ? "icp0.io" : "localhost";
-export const http_host = environment === "production" ? "icp0.io" : `http://localhost:${replica_port}`;
+const replica_port = environment === "ic" ? 4943 : 8000;
+const replica_host = environment === "ic" ? "icp0.io" : "localhost";
+export const http_host = environment === "ic" ? "icp0.io" : `http://localhost:${replica_port}`;
 export const frontend_canister_id = process.env.CANISTER_ID_MIGRATOR_MANAGEMENT_CANISTER_FRONTEND || "";
 export const backend_canister_id = process.env.CANISTER_ID_MIGRATOR_MANAGEMENT_CANISTER_BACKEND || "";
 export const icp_ledger_canister_id = process.env.CANISTER_ID_ICP_LEDGER_CANISTER || "";
@@ -15,7 +26,7 @@ export const internet_identity_canister_id = process.env.CANISTER_ID_INTERNET_ID
 
 export const frontend_canister_id_url = environment === "local" ? `http://${frontend_canister_id}.${replica_host}:${replica_port}` : `https://${frontend_canister_id}.icp0.io/`;
 export const ledger_canister_id_url = environment === "local" ? `http://${icp_ledger_canister_id}.${replica_host}:${replica_port}` : `https://${icp_ledger_canister_id}.icp0.io/`;
-export const internet_identity_canister_id_url = environment === "local" ? `http://${internet_identity_canister_id}.${replica_host}:${replica_port}` : `https://${internet_identity_canister_id}.icp0.io/`;
+export const internet_identity_canister_id_url = environment === "local" ? `http://${internet_identity_canister_id}.${replica_host}:${replica_port}` : `https://identity.ic0.app`;
 
 export const internetIdentityConfig = {
     identityProvider: internet_identity_canister_id_url,
@@ -27,10 +38,10 @@ export const internetIdentityConfig = {
 export const githubClientId = process.env.REACT_APP_GITHUB_CLIENT_ID || "";
 export const githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET || ""; // TODO: Remove this
 
-export const ngrok_tunnel = "https://0c21b4736015.ngrok.app";
+export const ngrok_tunnel = "https://1abfa900377c.ngrok.app";
 // export const reverse_proxy_url = "https://cors-anywhere.herokuapp.com";
 // export const reverse_proxy_url = "https://proxy.cors.sh";
-export const reverse_proxy_url = "http://localhost:8080";
-export const frontend_url = environment === "production" ? `https://${frontend_canister_id}.icp0.io` : ngrok_tunnel;
+export const reverse_proxy_url = environment === "ic" ? process.env.REACT_APP_REVERSE_PROXY_REMOTE_URL : "http://localhost:8080";
+export const frontend_url = environment === "ic" ? `https://${frontend_canister_id}.icp0.io` : ngrok_tunnel;
 
 export const cors_sh_api_key = process.env.REACT_APP_CORS_SH_API_KEY || "";
