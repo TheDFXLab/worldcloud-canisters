8aca96fc7073af282afa6eefd6f05caa2768febe
georgiod9 <georgiod9@gmail.com>
2025-02-20 17:14:11 +0400

    update ic assets for dev

--- src/migrator-management-canister-frontend/vite.config.js
+++ src/migrator-management-canister-frontend/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { fileURLToPath, URL } from 'url';
 import environment from 'vite-plugin-environment';
 import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 import dotenv from 'dotenv';
 
 dotenv.config({ path: '../../.env' });
@@ -21,6 +22,7 @@ export default defineConfig({
     },
   },
   server: {
+    allowedHosts: ["1abfa900377c.ngrok.app"],
     proxy: {
       "/api": {
         target: "http://0.0.0.0:8000",
@@ -34,6 +36,14 @@ export default defineConfig({
     environment("all", { prefix: "CANISTER_" }),
     environment("all", { prefix: "DFX_" }),
     environment("all", { prefix: "REACT_APP_" }),
+    viteStaticCopy({
+      targets: [
+        {
+          src: '../assets/.ic-assets.json5',
+          dest: ''
+        },
+      ]
+    })
   ],
   resolve: {
     alias: [
