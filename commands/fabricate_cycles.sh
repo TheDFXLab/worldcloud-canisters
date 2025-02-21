5d858a953765078804df354ac3e1ef23d4c31458
georgiod9 <georgiod9@gmail.com>
2025-02-21 13:50:55 +0400

    read canister id from dfx

--- commands/fabricate_cycles.sh
+++ commands/fabricate_cycles.sh
@@ -5,6 +5,7 @@ echo "===========FABRICATE CYCLES==========="
 echo ""
 echo "===========AMOUNT IN ICP==========="
 
-CANISTER_ID="be2us-64aaa-aaaaa-qaabq-cai"
+CANISTER_ID=$(dfx canister id migrator-management-canister-backend)
+echo "CANISTER_ID: $CANISTER_ID"
 AMOUNT_IN_ICP_TOKEN="100"
 dfx ledger fabricate-cycles --canister $CANISTER_ID --amount $AMOUNT_IN_ICP_TOKEN
