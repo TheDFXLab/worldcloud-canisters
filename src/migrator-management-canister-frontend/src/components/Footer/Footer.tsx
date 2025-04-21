dce1e2da1de0b6d28ac39102944c08214be45567
georgiod9 <georgiod9@gmail.com>
2025-04-21 11:57:28 +0400

    minor change

--- src/migrator-management-canister-frontend/src/components/Footer/Footer.tsx
+++ src/migrator-management-canister-frontend/src/components/Footer/Footer.tsx
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom";
 import "./Footer.css";
-import { getCanisterUrl } from "../../config/config";
 
 const Footer = () => {
   const currentDate = new Date().getFullYear();
