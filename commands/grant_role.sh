7948b892470451fae51e5eeace7a28ccc7abd405
georgiod9 <georgiod9@gmail.com>
2025-06-07 13:02:06 +0400

    specify environment in grant role script

--- commands/grant_role.sh
+++ commands/grant_role.sh
@@ -1,15 +1,27 @@
 
 echo "===========GRANT ROLE==========="
+# Usage
+# sh grant_role <Principal> <Role> <Env>
+# Principal: Target principal
+# Role: "super_admin" or "admin"
+# Env: "local" or "ic"
 
 #$1: principal
 #$2: role: "super_admin" or "admin"
+PRINCIPAL=$1
+ROLE=$2
+ENV=$3
 
 if [ -z "$1" ]; then
-    PRINCIPAL="ehxcm-jn7zo-4c6av-wnmv6-vgck3-yynmq-k22ho-guzre-ena2y-r7k5v-5qe"
+    PRINCIPAL="dk6i4-moydz-ajdjs-ho2b3-t77s2-li5nu-fbbjg-ykhfe-ki6ny-5alu5-cae"
 fi
 
 if [ -z "$2" ]; then
     ROLE="admin"
+fi
+
+if [-z "$3"]; then
+    ENV="local"
 fi
 
 if [ "$2" != "super_admin" ] && [ "$2" != "admin" ]; then
@@ -18,4 +30,4 @@ fi
 
 echo "Granting role $ROLE to principal $PRINCIPAL"
 
-dfx canister call migrator-management-canister-backend grant_role '(principal "'$PRINCIPAL'", variant { "'$ROLE'" })'
+dfx canister call migrator-management-canister-backend --network $ENV grant_role '(principal "'$PRINCIPAL'", variant { "'$ROLE'" })'
