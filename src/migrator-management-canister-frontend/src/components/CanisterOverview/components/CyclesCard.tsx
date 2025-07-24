cdc20dffcb6f0ba87b43c1ac0f33884a670b9d91
georgiod9 <georgiod9@gmail.com>
2025-07-24 21:37:58 +0400

    - convert cycles to terra

--- src/migrator-management-canister-frontend/src/components/CanisterOverview/components/CyclesCard.tsx
+++ src/migrator-management-canister-frontend/src/components/CanisterOverview/components/CyclesCard.tsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { CircularProgress } from "@mui/material";
 import AccountBalanceWalletIcon from "@mui/icons-material/AccountBalanceWallet";
-import { fromE8sStable } from "../../../utility/e8s";
+import { cyclesToTerra, fromE8sStable } from "../../../utility/e8s";
+import { useCyclesLogic } from "../../../hooks/useCyclesLogic";
 
 interface CyclesCardProps {
+  isFreemium: boolean;
   isLoadingBalance: boolean;
+  isLoadingAddCycles: boolean;
   balance: bigint | null | undefined;
   isLoadingCycles: boolean;
   cyclesStatus: any;
@@ -13,13 +16,33 @@ interface CyclesCardProps {
 }
 
 export const CyclesCard: React.FC<CyclesCardProps> = ({
+  isFreemium,
   isLoadingBalance,
+  isLoadingAddCycles,
   balance,
   isLoadingCycles,
   cyclesStatus,
   isLoadingEstimateCycles,
   maxCyclesExchangeable,
 }) => {
+  const renderPaidContent = () => {
+    return (
+      <>
+        {!isFreemium && (
+          <div className="info-row">
+            <div className="info-label">Add Cycles</div>
+            <div className="info-value">
+              {isLoadingAddCycles ? (
+                <CircularProgress size={20} />
+              ) : (
+                `${fromE8sStable(balance || 0n)} ICP`
+              )}
+            </div>
+          </div>
+        )}
+      </>
+    );
+  };
   return (
     <div className="overview-card">
       <div className="card-header">
@@ -29,7 +52,7 @@ export const CyclesCard: React.FC<CyclesCardProps> = ({
       <div className="card-content">
         <div className="info-table">
           <div className="info-row">
-            <div className="info-label">Available Balance</div>
+            <div className="info-label">Wallet Balance</div>
             <div className="info-value">
               {isLoadingBalance ? (
                 <CircularProgress size={20} />
@@ -39,12 +62,12 @@ export const CyclesCard: React.FC<CyclesCardProps> = ({
             </div>
           </div>
           <div className="info-row">
-            <div className="info-label">Current Cycles</div>
+            <div className="info-label">Canister Cycles</div>
             <div className="info-value">
               {isLoadingCycles ? (
                 <CircularProgress size={20} />
               ) : (
-                `${cyclesStatus?.cycles || 0} T Cycles`
+                `${cyclesToTerra(cyclesStatus?.cycles) || 0} T Cycles`
               )}
             </div>
           </div>
@@ -54,7 +77,7 @@ export const CyclesCard: React.FC<CyclesCardProps> = ({
               {isLoadingEstimateCycles ? (
                 <CircularProgress size={20} />
               ) : (
-                `${maxCyclesExchangeable || 0} T Cycles`
+                `${cyclesToTerra(maxCyclesExchangeable) || 0} T Cycles`
               )}
             </div>
           </div>
