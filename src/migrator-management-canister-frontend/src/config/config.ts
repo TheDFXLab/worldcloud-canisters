3dc1d39aeeea4e72f29a062d6b1efcdcbf879e9c
georgiod9 <georgiod9@gmail.com>
2025-09-15 18:17:55 +0400

    minor changes

--- src/migrator-management-canister-frontend/src/config/config.ts
+++ src/migrator-management-canister-frontend/src/config/config.ts
@@ -41,7 +41,7 @@ export const internetIdentityConfig = {
 export const githubClientId = environment == "ic" ? process.env.REACT_APP_GITHUB_CLIENT_ID || "" : _dev_env.github_client_id;
 export const githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET || ""; // TODO: Remove this
 
-export const ngrok_tunnel = "https://a2756d34b554.ngrok.app";
+export const ngrok_tunnel = "https://b561db5a449e.ngrok.app";
 export const reverse_proxy_url = environment === "ic" ? process.env.REACT_APP_REVERSE_PROXY_REMOTE_URL : "http://localhost:8080";
 // export const reverse_proxy_url = process.env.REACT_APP_REVERSE_PROXY_REMOTE_URL || "";
 export const frontend_url = environment === "ic" ? `https://${frontend_canister_id}.icp0.io` : ngrok_tunnel;
