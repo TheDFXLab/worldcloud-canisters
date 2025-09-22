b958adbc9ba92cb9b950b2902a7d3051e2278e61
georgiod9 <georgiod9@gmail.com>
2025-09-22 15:16:15 +0400

    minor changes

--- src/migrator-management-canister-frontend/vite.config.js
+++ src/migrator-management-canister-frontend/vite.config.js
@@ -42,7 +42,7 @@ export default defineConfig({
   },
   server: {
     port: 3000,
-    allowedHosts: ["965c7e5ecb0e.ngrok.app"],
+    allowedHosts: ["4c5d94974886.ngrok.app"],
     proxy: {
       "/api": {
         target: "http://0.0.0.0:8000",
